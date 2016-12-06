2040630e2c5af541c9407ea23d5b389e5a70a6ed
Aaron Stewart <aaron@workhabit.com>
2016-12-05 19:19:28 -0800

    Set GPIO as default profile, fixed unit tests

--- st-notifier-lightwall/src/test/java/org/menagerie/stnotifier/test/GPIORenderTargetTest.java
+++ st-notifier-lightwall/src/test/java/org/menagerie/stnotifier/test/GPIORenderTargetTest.java
@@ -53,58 +53,61 @@ public class GPIORenderTargetTest
 
                 will(returnValue(mockController));
 
-                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_04, "COM0", PinState.LOW);
+                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_07, "COM0", PinState.HIGH);
                 will(returnValue(com0Pin));
-                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_17, "COM1", PinState.LOW);
+                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_00, "COM1", PinState.HIGH);
                 will(returnValue(com1Pin));
-                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_27, "COM2", PinState.LOW);
+                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_02, "COM2", PinState.HIGH);
                 will(returnValue(com2Pin));
-                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_22, "COM3", PinState.LOW);
+                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_03, "COM3", PinState.HIGH);
                 will(returnValue(com3Pin));
 
-                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_14, "A0", PinState.LOW);
+                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_15, "A0", PinState.LOW);
                 will(returnValue(a0Pin));
-                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_15, "A1", PinState.LOW);
+                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_16, "A1", PinState.LOW);
                 will(returnValue(a1Pin));
-                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_18, "A2", PinState.LOW);
+                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_01, "A2", PinState.LOW);
                 will(returnValue(a2Pin));
-                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_23, "A3", PinState.LOW);
+                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_04, "A3", PinState.LOW);
                 will(returnValue(a3Pin));
-                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_24, "A4", PinState.LOW);
+                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_05, "A4", PinState.LOW);
                 will(returnValue(a4Pin));
-                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_25, "A5", PinState.LOW);
+                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_06, "A5", PinState.LOW);
                 will(returnValue(a5Pin));
-                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_08, "A6", PinState.LOW);
+                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_10, "A6", PinState.LOW);
                 will(returnValue(a6Pin));
-                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_07, "A7", PinState.LOW);
+                oneOf(mockController).provisionDigitalOutputPin(RaspiPin.GPIO_11, "A7", PinState.LOW);
                 will(returnValue(a7Pin));
 
-                oneOf(com0Pin).high();
+                // expectations for character "f"
+                oneOf(com0Pin).low();
                 oneOf(a5Pin).high();
-                never(com1Pin).high();
-                never(com2Pin).high();
-                never(com3Pin).high();
+
+                // expect that none of the other pins are set
+                never(com1Pin).low();
+                never(com2Pin).low();
+                never(com3Pin).low();
                 never(a0Pin).high();
                 never(a1Pin).high();
                 never(a2Pin).high();
                 never(a3Pin).high();
                 never(a4Pin).high();
-
                 never(a6Pin).high();
                 never(a7Pin).high();
 
-                oneOf(com0Pin).low();
-                oneOf(com1Pin).low();
-                oneOf(com2Pin).low();
-                oneOf(com3Pin).low();
-                oneOf(a0Pin).low();
-                oneOf(a1Pin).low();
-                oneOf(a2Pin).low();
-                oneOf(a3Pin).low();
-                oneOf(a4Pin).low();
-                oneOf(a5Pin).low();
-                oneOf(a6Pin).low();
-                oneOf(a7Pin).low();
+                // expectations for clear all (setAllOff)
+                exactly(2).of(com0Pin).high();
+                exactly(2).of(com1Pin).high();
+                exactly(2).of(com2Pin).high();
+                exactly(2).of(com3Pin).high();
+                exactly(2).of(a0Pin).low();
+                exactly(2).of(a1Pin).low();
+                exactly(2).of(a2Pin).low();
+                exactly(2).of(a3Pin).low();
+                exactly(2).of(a4Pin).low();
+                exactly(2).of(a5Pin).low();
+                exactly(2).of(a6Pin).low();
+                exactly(2).of(a7Pin).low();
 
             }
         });
